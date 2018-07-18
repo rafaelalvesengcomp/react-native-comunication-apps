471795e57d9c6f1d6ed49e2886d18346a94ef60f
Rafael Alves <rafaelalvesufc@gmail.com>
2018-07-18 15:49:43 -0300

    Adicao nos index

--- index.android.js
+++ index.android.js
@@ -5,9 +5,9 @@ import { NativeModules } from 'react-native'
 
 const ComunicationApps = {};
 
-ComunicationApps.setup = (name = '', packagesName = '', title = '', progress = 0, tags = '', isAudio = 0, isVideo = 0) =>{
+ComunicationApps.setup = (name = '', packagesName = '', title = '', thumbnail = '', background = '', progress = 0, tags = '', isAudio = 0, isVideo = 0) =>{
     NativeModules.ComunicationApps.setup(
-        name, packagesName, title, progress, tags, isAudio, isVideo
+        name, packagesName, title, thumbnail, background, progress, tags, isAudio, isVideo
     );
 }
 
@@ -15,8 +15,9 @@ ComunicationApps.getInformations = () => {
     NativeModules.ComunicationApps.getInformations();
 }
 
-ComunicationApps.update = (name, packagesName, title, progress, tags, isAudio, isVideo) => {
-    NativeModules.ComunicationApps.update(name, packagesName, title, progress, tags, isAudio, isVideo);
+ComunicationApps.update = (name, packagesName, title, thumbnail, background, progress, tags, isAudio, isVideo) => {
+    NativeModules.ComunicationApps.update(name, packagesName, title, thumbnail, background, progress, tags, isAudio, isVideo);
 }
+
 
 export default ComunicationApps;
